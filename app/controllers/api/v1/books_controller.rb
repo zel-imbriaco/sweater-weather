45e5b1811df719406de09e7e14005bbad9333053
Zachary Imbriaco <zachary@imbriaco.com>
2022-06-13 13:30:14 -0400

    Refactor: Moved redundant if block into && block one level above.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,12 +1,8 @@
 class Api::V1::BooksController < ApplicationController
   def search
-    if params[:location] && params[:quantity]
-      if params[:quantity].to_i > 0
+    if params[:location] && params[:quantity] && params[:quantity].to_i > 0
         response = BookService.book_search(params[:location], params[:quantity])
         render json: BookSearchSerializer.book_search(response, params[:location], params[:quantity])
-      else
-        render status: :bad_request
-      end
     else
       render status: :bad_request
     end
