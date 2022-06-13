6b3fc8a45e6d31734211708d2a8cf882e2112a04
Zachary Imbriaco <zachary@imbriaco.com>
2022-06-13 11:56:26 -0400

    Tests: Create book-search route tests for book finding.

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,3 +1,9 @@
 FactoryBot.define do
   factory :book do
-    isbn: [Faker::Number.number(digits: 10), Faker::Number.number(digits(13))]
+    isbn { [Faker::Number.number(digits: 10), Faker::Number.number(digits: 13)] }
+    subject { Faker::Book.genre }
+    title { Faker::Book.title }
+    author { Faker::Book.author }
+    publisher { Faker::Book.publisher }
+  end
+end
--- spec/requests/books/book-search_spec.rb
+++ spec/requests/books/book-search_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
-RSpec.descrbe 'Book Search', type: :request do
+RSpec.describe 'Book Search', type: :request do
 
   describe 'GET /api/v1/book-search' do
     before do
       @book1 = create(:book, isbn: [1234567890, 1234567891011], subject: 'Professional Wrestling', title: 'Wrasslin', author: 'Sting', publisher: 'AEW Books')
       @book2 = create(:book, isbn: [2345678910, 2345678910111], subject: 'Professional Wrestling', title: 'Sports Entertainin', author: 'Vincent K. McMahon', publisher: 'WWE Books')
       @book3 = create(:book)
+      get "api/v1/book-search?location=Professional Wrestling&quantity=2"
     end
-    
+
+    it 'returns 2xx response' do
+      expect(response).to have_http_status :success
+    end
+
+    it 'returns 4xx response for invalid request' do
+      get "/api/v1/book-search"
+      expect(response).to have_http_status :failure
+    end
+
+    it 'Returns data of type books' do
+      expect(response["data"]["type"]).to eq "books"
+    end
+
+    it 'returns the correct number of books' do
+      expect(response["data"]["attributes"]["books"].count).to eq 2
+    end
+  end
 end
