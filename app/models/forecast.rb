86f2027a9dbf8383114d146ee3b9997994d846a7
Zachary Imbriaco <zachary@imbriaco.com>
2022-06-14 15:56:03 -0400

    Model tested and passing.

--- app/models/forecast.rb
+++
@@ -1,2 +0,0 @@
-def Forecast
-end
--- spec/requests/users/get_users_requests_spec.rb
+++ spec/requests/users/get_users_requests_spec.rb
@@ -3,8 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
 
   describe 'POST' do
-    describe '/api/v1/users' do
-      
+    describe 'user registration' do
+      before do
+        post '/api/v1/users', params: {
+          payload: {
+          email: "fuzzy@duck.com",
+          password: "DuckyFuzz1",
+          password_confirmation: "DuckyFuzz1"
+          }
+        }
+      end
+
+      it 'Returns 201 response on successful user registration' do
+        expect(response).to have_http_status 201
+      end
+
+      it 'Adds the newly created user to the database' do
+        
     end
   end
 end
