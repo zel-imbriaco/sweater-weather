49c2a822ddbe5488f79cd0406b6dad6607b772a1
Zachary Imbriaco <zachary@imbriaco.com>
2022-06-15 09:56:52 -0400

    Fixed authentication testing for login sessions.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,12 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if user.class != User
+      render status: 401
+    elsif user.authenticate(params[:password])
       render status: 200, json: UserSerializer.new(user)
+    else
+      render status: 401
     end
   end
 end
--- spec/requests/sessions/sessions_requests_spec.rb
+++ spec/requests/sessions/sessions_requests_spec.rb
@@ -15,5 +15,19 @@ RSpec.describe 'session requests', type: :request do
       expect(json["data"]["attributes"]["email"]).to eq "fuzzy@duck.com"
       expect(json["data"]["attributes"]["api_key"]).to eq @user.api_key
     end
+
+    it 'Returns a 401 error if credentials do not authenticate' do
+      post "/api/v1/sessions", params: {
+        email: "ducky@fuzz.com",
+        password: "fuzzyDuck1"
+      }
+      expect(response).to have_http_status 401
+
+      post "/api/v1/sessions", params: {
+        email: "fuzzy@duck.com",
+        password: "FuzzyDuck1"
+      }
+      expect(response).to have_http_status 401
+    end
   end
 end
